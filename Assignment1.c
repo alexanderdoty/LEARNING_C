c202ac1acdf692a8e01f9ec0e1bf9ac8400ac234
alexdoty <adoty4@gmail.com>
2022-01-13 06:49:44 -0600

    Add files via upload

---
+++ Assignment1.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  LearningC
+//
+//  Created by Alex Doty on 1/13/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+	return 0;
+}
